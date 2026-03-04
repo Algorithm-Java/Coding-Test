ee619296c125e783bce71a965b1ecbfa37e4e898
ChAnGMiNBae <qockdals752@gmail.com>
2026-03-04 19:21:32 +0900

    #5 Changmin 백준 10866번 덱

--- 2주차/덱/덱.java
+++ 2주차/덱/덱.java
@@ -1,4 +1,4 @@
-package 큐;
+package 덱;
 
 import java.io.*;
 import java.util.*;
@@ -10,49 +10,62 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
 
-        int[] q = new int[n];
+        int cap = n + 1;
+        int[] dq = new int[cap];
         int head = 0;
         int tail = 0;
         int size = 0;
+
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String cmd = st.nextToken();
 
             switch (cmd) {
-                case "push": {
+                case "push_front":
                     int x = Integer.parseInt(st.nextToken());
-                    q[tail] = x;
-                    tail = (tail + 1) % n;
+                    head = (head - 1 + cap) % cap;
+                    dq[head] = x;
                     size++;
                     break;
-                }
-                case "pop": {
-                    if (size == 0) sb.append(-1).append('\n');
+                case "push_back":
+                    x = Integer.parseInt(st.nextToken());
+                    dq[tail] = x;
+                    tail = (tail + 1) % cap;
+                    size++;
+                    break;
+                case "pop_front":
+                    if(size == 0)
+                        sb.append(-1).append('\n');
                     else {
-                        sb.append(q[head]).append('\n');
-                        head = (head + 1) % n;
+                        sb.append(dq[head]).append('\n');
+                        head = (head + 1) % cap;
                         size--;
                     }
                     break;
-                }
+                case "pop_back":
+                    if(size == 0)
+                        sb.append(-1).append('\n');
+                    else {
+                        tail = (tail - 1 + cap) % cap;
+                        sb.append(dq[tail]).append('\n');
+                        size--;
+                    }
+                    break;
                 case "size":
                     sb.append(size).append('\n');
                     break;
-
                 case "empty":
                     sb.append(size == 0 ? 1 : 0).append('\n');
                     break;
-
                 case "front":
-                    sb.append(size == 0 ? -1 : q[head]).append('\n');
+                    sb.append(size == 0 ? -1 : dq[head]).append('\n');
                     break;
-
                 case "back":
                     if (size == 0) sb.append(-1).append('\n');
                     else {
-                        int backIdx = (tail - 1 + n) % n;
-                        sb.append(q[backIdx]).append('\n');
+                        int back = (tail - 1 + cap) % cap;
+                        sb.append(dq[back]).append('\n');
                     }
                     break;
             }
