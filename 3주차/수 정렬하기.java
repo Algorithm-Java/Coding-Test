a849f0cf42c6350c72a4833cbb90a3081c298e36
KyujinPark01 <rbwls1100@naver.com>
2026-03-10 21:33:32 +0900

    #13 수 정렬하기

---
+++ 3주차/수 정렬하기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int[] arr = new int[A];
+        for (int i = 0; i < A; i++) {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
